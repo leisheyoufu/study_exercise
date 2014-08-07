866e96b3d69681aaef00e3835c1201b2094f1271
chenglch <chenglch@cn.ibm.com>
2014-08-07 01:09:10 -0400

    python unittest example

---
+++ python/unittest/myclass.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+__author__ = 'chenglong'
+
+
+def sum( x, y):
+        return x+y
+
+
+def sub( x, y):
+        return x-y
---
+++ python/unittest/test_myclass.py
@@ -0,0 +1,20 @@
+__author__ = 'chenglong'
+
+import unittest
+import myclass
+
+class mytest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testsum(self):
+        self.assertEqual(myclass.sum(1, 2), 1, 'test sum fail')
+
+    def testsub(self):
+        self.assertEqual(myclass.sub(2, 1), 1, 'test sub fail')
+
+if __name__ =='__main__':
+    unittest.main()
