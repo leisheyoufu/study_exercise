3b20565623c5747850e5d8e0488a455ca34d5192
leisheyoufu <clscu@live.cn>
2016-12-15 22:42:50 +0800

    leetcode 95. Unique Binary Search Trees II

---
+++ algorithm/leetcode/95/main.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<conio.h>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	vector<TreeNode*> generate(int left, int right)
+	{
+		vector<TreeNode* > ret;
+		if(left>right) {
+			ret.push_back(NULL);
+		}
+		for(int k=left;k<=right;k++) {
+			vector<TreeNode*> leftTree = generate(left, k-1);
+			vector<TreeNode*> rightTree = generate(k+1, right);			
+			for(int i=0;i<leftTree.size();i++) {
+				for(int j=0;j<rightTree.size();j++) {
+					TreeNode *root = new TreeNode(k);
+					root->left = leftTree[i];
+					root->right = rightTree[j];
+					ret.push_back(root);
+				}
+			}
+		}
+		return ret;
+	}
+
+    vector<TreeNode*> generateTrees(int n) {
+		if (n==0) {
+			vector<TreeNode* > ret;
+			return ret;
+		}
+		return generate(1,n);
+    }
+};
+
+int main()
+{
+	system("pause");
+    return 0;
+}
