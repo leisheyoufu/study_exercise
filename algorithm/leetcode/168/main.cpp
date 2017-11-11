507fa7d05adf84fdd5f9d7535ffdf4ffbc190440
leisheyoufu <clscu@live.cn>
2017-11-11 17:21:13 +0800

    leetcode 168 Excel Sheet Column Title

---
+++ algorithm/leetcode/168/main.cpp
@@ -0,0 +1,48 @@
+/*
+168. Excel Sheet Column Title
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include <sstream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string convertToTitle(int n)
+    {
+        if( n<= 0) {
+            return "";
+        }
+        string ret;
+        while(n) {
+            ret += (n-1) % 26 + 'A';
+            n = (n-1)/ 26;
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
+
+int main()
+{
+    Solution sln;
+    cout << sln.convertToTitle(27) << endl;
+    system("pause");
+    return 0;
+}
