7f3d0facaf6b95481df7fda67178aee09c2d11a3
leisheyoufu <clscu@live.cn>
2018-01-07 19:40:11 +0800

    leetcode 9 Palindrome Number, no extra space

--- algorithm/leetcode/9/main.cpp
+++ algorithm/leetcode/9/main.cpp
@@ -16,31 +16,30 @@ There is a more generic way of solving this problem.
 #include<iostream>
 #include<string>
 #include<algorithm>
-#include<conio.h>
 
 using namespace std;
-const int MAX = 0x7fffffff;
-const int MIN = 0x80000000;
 class Solution
 {
 public:
 
     bool isPalindrome(int x)
     {
-        int a[100];
-        int k =0, i = 0;
-        if (x<0) {
+        if(x < 0) {
             return false;
         }
-        while(x) {
-            a[k++] = x%10;
-            x/=10;
+        int a = 1;
+        int temp = x;
+        while(temp>=10) {
+            temp /= 10;
+            a *= 10;
         }
-        while( i< k/2) {
-            if(a[i] != a[k-1-i]) {
+        while( a > 1) {
+            if( (x / a) != (x % 10)) {
                 return false;
             }
-            i++;
+            x %= a;
+            x /= 10;
+            a /= 100;
         }
         return true;
     }
@@ -48,7 +47,11 @@ public:
 int main()
 {
     Solution sln;
-    printf("%d\n",sln.isPalindrome(-2147447412));
-    getch();
+    cout << sln.isPalindrome(11)<< endl;
+    cout << sln.isPalindrome(99099) << endl;
+    cout << sln.isPalindrome(987789) << endl;
+    cout << sln.isPalindrome(-2147447412) << endl;
+    cout << sln.isPalindrome(-2147483648) << endl;
+    system("pause");
     return 0;
 }
