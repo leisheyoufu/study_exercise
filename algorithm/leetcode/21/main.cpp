c97aeeb20e66a6ecd832ed2fafff074c57307210
leisheyoufu <clscu@live.cn>
2018-01-28 19:46:23 +0800

    leetcode 21 Merge Two Sorted Lists

--- algorithm/leetcode/21/main.cpp
+++ algorithm/leetcode/21/main.cpp
@@ -22,35 +22,33 @@ class Solution
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
     {
-        ListNode *head = NULL, *temp = NULL;
-        if(l1 && l2) {
-            if(l1->val < l2->val) {
-                head = l1;
-                l1 = l1->next;
-            } else {
-                head = l2;
-                l2 = l2->next;
-            }
-        } else if(!l1) {
+        if(!l1) {
             return l2;
-        } else if(!l2) {
+        }
+        if(!l2) {
             return l1;
         }
-        temp = head;
-        while(l1!=NULL || l2!=NULL) {
+        ListNode *head = l1;
+        if(l2->val < head->val) {
+            head = l2;
+            l2 = l2->next;
+        } else {
+            l1 = l1->next;
+        }
+        ListNode *temp = head;
+        while(l1 || l2) {
             if(!l1) {
                 temp->next = l2;
                 return head;
-            }
-            if(!l2) {
+            } else if(!l2) {
                 temp->next = l1;
                 return head;
             }
-            if(l1->val <l2->val) {
+            if(l1->val < l2->val) {
                 temp->next = l1;
                 l1 = l1->next;
             } else {
-                temp->next = l2;
+                temp ->next = l2;
                 l2 = l2->next;
             }
             temp = temp->next;
