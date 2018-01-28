762543f399c876acb5bd4781f9422361443b0e1a
leisheyoufu <clscu@live.cn>
2018-01-28 20:20:57 +0800

    leetcode 22 Generate Parentheses

--- algorithm/leetcode/22/main.cpp
+++ algorithm/leetcode/22/main.cpp
@@ -25,6 +25,7 @@ using namespace std;
 class Solution
 {
 public:
+    /*
     void generate(int left, int right, string str, vector<string> &res)
     {
         if(left>right) {
@@ -49,10 +50,33 @@ public:
         generate(n,n,"",res);
         return res;
     }
+    */
+    void generate(int cnt, int n, string curr, vector<string> &ret)
+    {
+        if(n == 0) {
+            ret.push_back(curr);
+            return;
+        }
+        if(cnt==0) {
+            generate(cnt+1, n-1, curr + '(', ret);
+        } else if(cnt == n) {
+            generate(cnt-1, n-1, curr + ')', ret);
+        } else {
+            generate(cnt+1, n-1, curr + '(', ret);
+            generate(cnt-1, n-1, curr + ')', ret);
+        }
+    }
+    vector<string> generateParenthesis(int n)
+    {
+        vector<string> ret;
+        generate(0, n*2, "", ret);
+        return ret;
+    }
 };
 int main()
 {
     Solution sln;
+    sln.generateParenthesis(3);
     system("pause");
     return 0;
 }
