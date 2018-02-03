d13ae019f4a572d64e18ffa7bc2d7c9aae82017a
leisheyoufu <clscu@live.cn>
2018-02-03 19:22:09 +0800

    leetcode 26 Remove Duplicates from Sorted Array

--- algorithm/leetcode/26/main.cpp
+++ algorithm/leetcode/26/main.cpp
@@ -1,4 +1,4 @@
-/* Remove Duplicates from Sorted Array
+/* 26. Remove Duplicates from Sorted Array
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 
 Do not allocate extra space for another array, you must do this in place with constant memory.
@@ -16,21 +16,11 @@ Your function should return length = 2, with the first two elements of nums bein
 #include<cstdlib>
 using namespace std;
 
-/*
-Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
-
-Do not allocate extra space for another array, you must do this in place with constant memory.
-
-For example,
-Given input array nums = [1,1,2],
-
-Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
-*/
-
 class Solution
 {
 public:
-    int removeDuplicates(vector<int>& nums)
+    /*
+        int removeDuplicates(vector<int>& nums)
     {
         int n = nums.size();
         if(n==0) {
@@ -42,6 +32,23 @@ public:
                 nums[j++] = nums[i];
             }
             i++;
+        }
+        return j;
+    }
+    */
+    int removeDuplicates(vector<int>& nums)
+    {
+        if(nums.empty()) {
+            return 0;
+        }
+        int n = nums.size();
+        int i = 1, j =1;
+        while(i<n) {
+            if(nums[i] == nums[i-1]) {
+                ++i;
+                continue;
+            }
+            nums[j++] = nums[i++];
         }
         return j;
     }
