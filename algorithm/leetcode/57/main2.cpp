d0175c54700cb5fb2dab19dfa353f627dcd2743f
leisheyoufu <clscu@live.cn>
2018-03-20 10:59:22 +0800

    leetcode 57. Insert Interval

--- algorithm/leetcode/57/main2.cpp
+++ algorithm/leetcode/57/main2.cpp
@@ -33,41 +33,18 @@ int comp(Interval &val1, Interval &val2)
 class Solution
 {
 public:
-    bool overlap(Interval a, Interval b)
-    {
-        if(a.end >= b.start && a.start <=b.start || b.end >= a.start && b.start <=a.start
-           || a.start<=b.start && a.end >=b.end || b.start<=a.start && b.end >=a.end ) {
-            return true;
-        }
-        return false;
-    }
+
     vector<Interval> insert(vector<Interval>& intervals, Interval newInterval)
     {
         vector<Interval> ret;
         if(intervals.empty()) {
             return vector<Interval>(1, newInterval);
         }
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(), comp);
         Interval curr = intervals[0];
-        if (intervals.size() == 1) {
-            if(overlap(curr, newInterval)) {
-                curr.end = max(curr.end, newInterval.end);
-                curr.start = min(curr.start, newInterval.start);
-                ret.push_back(curr);
-            } else {
-                ret.push_back(curr);
-                ret.push_back(newInterval);
-            }
-            sort(ret.begin(), ret.end(), comp);
-            return ret;
-        }
-        bool merged = false;
         for(int i=1; i<intervals.size(); i++) {
-            if(!merged && overlap(curr, newInterval)) {
-                curr.end = max(curr.end, newInterval.end);
-                curr.start = min(curr.start, newInterval.start);
-                merged = true;
-            }
-            if(overlap(curr, intervals[i])) {
+            if(curr.end >= intervals[i].start) {
                 curr.end = max(curr.end, intervals[i].end);
                 curr.start = min(curr.start, intervals[i].start);
             } else {
@@ -75,18 +52,7 @@ public:
                 curr = intervals[i];
             }
         }
-        if (!merged) {
-            if (overlap(curr, newInterval)) {
-                curr.end = max(curr.end, newInterval.end);
-                curr.start = min(curr.start, newInterval.start);
-                ret.push_back(curr);
-            } else {
-                ret.push_back(newInterval);
-            }
-        } else {
-            ret.push_back(curr);
-        }
-        sort(ret.begin(), ret.end(), comp);
+        ret.push_back(curr);
         return ret;
     }
 };
@@ -94,14 +60,13 @@ int main()
 {
     Solution sln;
     vector<Interval> intervals;
-    Interval n1(1,5);
-    Interval n2(12,15);
-    Interval n3(6,6);
+    intervals.push_back(Interval (1,2));
+    intervals.push_back(Interval (3,5));
+    intervals.push_back(Interval (6,7));
+    intervals.push_back(Interval (8,10));
+    intervals.push_back(Interval (12,16));
     vector<Interval> ret;
-    ret = sln.insert(intervals, n1);
-    ret = sln.insert(ret, n2);
-    ret = sln.insert(ret, n3);
-
+    sln.insert(intervals, Interval (4,9));
     system("pause");
     return 0;
 }
