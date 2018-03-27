43034f03ec205ff8a00494b2994c4e0a0f24f25f
leisheyoufu <clscu@live.cn>
2018-03-27 16:16:47 +0800

    leetcode 75. Sort Colors

---
+++ algorithm/leetcode/75/main2.cpp
@@ -0,0 +1,71 @@
+/*
+75. Sort Colors
+
+Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note:
+You are not suppose to use the library's sort function for this problem.
+
+click to show follow up.
+
+Follow up:
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+
+Could you come up with an one-pass algorithm using only constant space?
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+
+using namespace std;
+
+class Solution
+{
+public:
+    // CL: the last solution is better
+    void sortColors(vector<int>& nums)
+    {
+        int n = nums.size();
+        if( n == 0) {
+            return;
+        }
+        int i=0, j=n, k=n;
+        int c = 0;
+        while( c<n) {
+            if(nums[i] == 0) {
+                i++;
+            } else if(nums[i] == 1) {
+                nums[i] = nums[--j];
+                nums[j] = 1;
+            } else if(nums[i] == 2) {
+                if(j!=k) {
+                    nums[i] = nums[--j];
+                    nums[--k] = 2;
+                    nums[j] = 1;
+                } else {
+                    nums[i] = nums[--j];
+                    nums[--k] = 2;
+                }
+            }
+            c++;
+        }
+    }
+
+};
+int main()
+{
+    Solution sln;
+    //int a[] = {1,2,0};
+    int a[] = {1,2,2,1,1,2,1};
+    vector<int> v(a, a+ sizeof(a)/sizeof(int));
+    sln.sortColors(v);
+    for(int i =0; i<v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    system("pause");
+    return 0;
+}
