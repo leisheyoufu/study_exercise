edbf86f42a92cadf465f595c43e18509b9055054
leisheyoufu <clscu@live.cn>
2018-04-29 18:36:51 +0800

    leetocde 209. Minimum Size Subarray Sum

---
+++ algorithm/leetcode/209/main.cpp
@@ -0,0 +1,46 @@
+/*
+209. Minimum Size Subarray Sum
+
+Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint.
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+#include<set>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    int minSubArrayLen(int s, vector<int>& nums)
+    {
+        int n = nums.size();
+        if(n == 0) {
+            return 0;
+        }
+        int minL = INT_MAX;
+        int left = 0;
+        int sum = nums[0];
+        for(int i=1; i<n; i++) {
+            sum += nums[i];
+            while(sum > s) {
+                minL = min(minL, i-left + 1);
+                sum -= nums[left++];
+            }
+        }
+
+
+    }
+};
+
+int main()
+{
+    Solution sln;
+    system("pause");
+    return 0;
+}
