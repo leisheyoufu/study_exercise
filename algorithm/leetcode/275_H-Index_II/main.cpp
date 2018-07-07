a2e6db5ddbb47e0b003e6ce51b495598686bdbc6
leisheyoufu <clscu@live.cn>
2018-07-07 19:06:42 +0800

    leetcode 275. H-Index II  binary search

---
+++ algorithm/leetcode/275_H-Index_II/main.cpp
@@ -0,0 +1,64 @@
+/*
+275. H-Index II
+
+Given an array of citations sorted in ascending order (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+Example:
+
+Input: citations = [0,1,3,5,6]
+Output: 3
+Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each of them had
+             received 0, 1, 3, 5, 6 citations respectively.
+             Since the researcher has 3 papers with at least 3 citations each and the remaining
+             two with no more than 3 citations each, her h-index is 3.
+Note:
+
+If there are several possible values for h, the maximum one is taken as the h-index.
+
+Follow up:
+
+This is a follow up problem to H-Index, where citations is now guaranteed to be sorted in ascending order.
+Could you solve it in logarithmic time complexity?
+*/
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int hIndex(vector<int>& citations)
+    {
+        int n = citations.size();
+        if(n == 0) {
+            return 0;
+        }
+        int left = 0, right = n-1, mid;
+        int ret = 0;
+        while(left <= right) {
+            mid = (left+right)/2;
+            if(citations[mid] >= n-mid) {
+                ret = n-mid;
+                right = mid - 1;
+            } else if(citations[mid] < n-mid) {
+                left = mid+1;
+            }
+        }
+        return ret;
+    }
+};
+int main()
+{
+    Solution sln;
+    int a[] = {0,1,3,5,6};
+    vector<int> v(a, a+ sizeof(a)/sizeof(int));
+    cout << sln.hIndex(v) << endl;
+    system("pause");
+    return 0;
+}
